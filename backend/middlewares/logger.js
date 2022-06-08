5b4df67aad69c6dba21b37582b0fe056f97b42c0
Alinazavalii20 <87885095+Alinazavalii20@users.noreply.github.com>
2022-06-08 17:00:56 +0300

    Add files via upload

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,7 +1,7 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
-//запросы
+// запросы
 const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.File({ filename: 'request.log' }),
@@ -9,7 +9,7 @@ const requestLogger = expressWinston.logger({
   format: winston.format.json(),
 });
 
-//ошибки
+// ошибки
 const errorLogger = expressWinston.errorLogger({
   transports: [
     new winston.transports.File({ filename: 'error.log' }),
@@ -17,7 +17,7 @@ const errorLogger = expressWinston.errorLogger({
   format: winston.format.json(),
 });
 
-module.exports ={
+module.exports = {
   requestLogger,
-  errorLogger
-}
+  errorLogger,
+};

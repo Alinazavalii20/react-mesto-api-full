540a0b87f9d213fb9bd0fada606c5688d05c103e
Alinazavalii20 <87885095+Alinazavalii20@users.noreply.github.com>
2022-06-19 22:12:22 +0300

    Add files via upload

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,16 +4,16 @@ const UnAuthtorizeError = require('../errors/UnAuthtorizeError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const authorization = req.header;
+  const { authorization } = req.header;
 
   if (!authorization) {
     next(new UnAuthtorizeError('Необходима авторизация'));
   } else {
-    const token = authorization;
+    const token = authorization.replace('Bearer ', '');
     let payload;
 
     try {
-      payload = jwt.verify(token, NODE_ENV !== 'production' ? JWT_SECRET : 'dev-secret');
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
     } catch (err) {
       next(new UnAuthtorizeError('Необходима авторизация'));
       return;

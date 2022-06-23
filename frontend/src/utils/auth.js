690874830e45c39da9432af2327dc96a37e31cbc
Alinazavalii20 <87885095+Alinazavalii20@users.noreply.github.com>
2022-06-24 00:41:34 +0300

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
 //import api from '../utils/Api';
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'http://api.alina.nomoreparties.sbs';
 
 function getResponse(res) {
     return res.status === '200' || '400' || '401'
